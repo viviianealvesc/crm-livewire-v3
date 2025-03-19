8db5555a404bc39188f7d2d680f8d32a3811b139
Viviane Alves <viviane.alvesc01@gmail.com>
2025-03-19 07:51:41 -0300

    create tests

--- tests/Feature/Livewire/LoginTest.php
+++ tests/Feature/Livewire/LoginTest.php
@@ -5,5 +5,22 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Login::class)
-        ->assertStatus(200);
+        ->assertOk();
+});
+
+it('should be able to login', function () {
+   $user = User::factory()->create([
+        'email' => 'johndoe@.com',
+        'password' => 'password',
+    ]);
+
+    Livewire::teste(Login::class)
+    ->set('email', 'johndoe@.com')
+    ->set('password', 'password')
+    ->call('tryToLogin')
+    ->assertHasNoErrors()
+    ->assertRedirect('/');
+
+    espect(auth()->check())->toBeTrue()
+    ->and(auth()->user())->id->toBe($user->id);
 });
